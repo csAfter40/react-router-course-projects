fc6873a0fae787ef3d24a9ede55a7dd1cd69efe3
csAfter40 <csafter40@gmail.com>
2023-06-09 16:35:50 +0300

    add page layout

--- vanlife/src/App.js
+++ vanlife/src/App.js
@@ -1,23 +1,22 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/vans" element={<Vans />}/>
-        <Route path="/vans/:id" element={<VanDetail />}/>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VanDetail />}/>
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
