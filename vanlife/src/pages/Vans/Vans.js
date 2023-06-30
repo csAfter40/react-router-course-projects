4293efe7220019486e926e1fe5ea6f0948bd5425
csAfter40 <csafter40@gmail.com>
2023-07-01 02:10:55 +0300

    implement defer on vans page

--- vanlife/src/pages/Vans/Vans.js
+++ vanlife/src/pages/Vans/Vans.js
@@ -1,20 +1,21 @@
 import React  from "react";
 import FilterButton from "../../components/FilterButton"
 import VanCard from "../../components/VanCard";
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { Link, useSearchParams, useLoaderData, defer, Await } from "react-router-dom";
 import { getVans } from "../../api";
 
 export function loader(){
-    return getVans("/api/vans");
+    const vansPromise = getVans("/api/vans");
+    return defer({vans: vansPromise});
 }
 
 export default function Vans() {
-    const vans = useLoaderData()
+    const loaderData = useLoaderData()
     const [searchParams, setSearchParams] = useSearchParams()
     const typeFilter = searchParams.get("type");
-    const filteredVansArray = typeFilter 
-        ? vans.filter(van=>van.type===typeFilter)
-        : vans;
+    // const filteredVansArray = typeFilter 
+    //     ? vans.filter(van=>van.type===typeFilter)
+    //     : vans;
     
     function editSearchParams(key, value) {
         setSearchParams((prevParams) => {
@@ -26,45 +27,59 @@ export default function Vans() {
     return (
         <div className="vans">
             <h2>Explore our van options</h2>
-            <div className="filter-button-container">
-                <FilterButton 
-                    isSelected={typeFilter==="simple"} 
-                    handleClick={()=>editSearchParams("type", "simple")} 
-                    name="Simple"
-                />
-                <FilterButton 
-                    isSelected={typeFilter==="luxury"} 
-                    handleClick={()=>editSearchParams("type", "luxury")} 
-                    name="Luxury"
-                />
-                <FilterButton 
-                    isSelected={typeFilter==="rugged"} 
-                    handleClick={()=>editSearchParams("type", "rugged")} 
-                    name="Rugged"
-                />
-                {typeFilter && <p onClick={()=>editSearchParams("type", null)}>Clear filters</p>}
-            </div>
-            <div className="cards-container">
-                {filteredVansArray.map((van, i)=>{
-                    return (
-                        <Link 
-                            key={i} 
-                            className="vans-detail-link" 
-                            to={van.id}
-                            state={{search: searchParams}}
-                        >
-                            <VanCard
-                                key={van.id}
-                                name={van.name}
-                                price={van.price}
-                                imageUrl={van.imageUrl}
-                                type={van.type}
-                            />
-                        </Link>
-                    )
-                })}
-
-            </div>
+            <React.Suspense fallback={ <h2>Loaing Vans</h2> }>
+                <Await resolve={loaderData.vans}>
+                    
+                    {(vans)=>{
+                        console.log(vans)
+                        const filteredVansArray = typeFilter 
+                            ? vans.filter(van=>van.type===typeFilter)
+                            : vans;
+                        return (
+                            <>
+                                <div className="filter-button-container">
+                                    <FilterButton 
+                                        isSelected={typeFilter==="simple"} 
+                                        handleClick={()=>editSearchParams("type", "simple")} 
+                                        name="Simple"
+                                    />
+                                    <FilterButton 
+                                        isSelected={typeFilter==="luxury"} 
+                                        handleClick={()=>editSearchParams("type", "luxury")} 
+                                        name="Luxury"
+                                        />
+                                    <FilterButton 
+                                        isSelected={typeFilter==="rugged"} 
+                                        handleClick={()=>editSearchParams("type", "rugged")} 
+                                        name="Rugged"
+                                        />
+                                    {typeFilter && <p onClick={()=>editSearchParams("type", null)}>Clear filters</p>}
+                                </div>  
+                                <div className="cards-container">
+                                    {filteredVansArray.map((van, i)=>{
+                                        return (
+                                            <Link 
+                                            key={i} 
+                                            className="vans-detail-link" 
+                                                to={van.id}
+                                                state={{search: searchParams}}
+                                            >
+                                                <VanCard
+                                                    key={van.id}
+                                                    name={van.name}
+                                                    price={van.price}
+                                                    imageUrl={van.imageUrl}
+                                                    type={van.type}
+                                                />
+                                            </Link>
+                                        )
+                                    })}
+                                </div>
+                            </>
+                        )
+                    }}
+                </Await>
+            </React.Suspense>
         </div>
     )
 }
