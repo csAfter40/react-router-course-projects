cc65fa2faada4a84fbb51bc1f2b8ec4346409ce4
csAfter40 <csafter40@gmail.com>
2023-07-02 09:44:58 +0300

    setup firebase

--- vanlife/src/server.js
+++ vanlife/src/server.js
@@ -24,6 +24,7 @@ createServer({
     routes() {
         this.namespace = "api"
         this.logging = false
+        this.passthrough("https://firestore.googleapis.com/**")
 
         this.get("/vans", (schema, request) => {
             // return new Response(400, {}, {error: "Error fetching data"})
